036ac6c56d928e7bb56d70551a80b587aa64c396
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-01-13 18:50:26 +0100

    progress

--- tools/numpydoc_lint.py
+++ tools/numpydoc_lint.py
@@ -1,54 +1,10 @@
 #!/usr/bin/env python
 import importlib
 import sys
+from pprint import pprint
+from types import ModuleType
 
 from numpydoc.validate import validate
-
-PUBLIC_MODULES = [
-    "scipy." + s
-    for s in [
-        "cluster",
-        "cluster.vq",
-        "cluster.hierarchy",
-        "constants",
-        "datasets",
-        "differentiate",
-        "fft",
-        "fftpack",
-        "integrate",
-        "interpolate",
-        "io",
-        "io.arff",
-        "io.matlab",
-        "io.wavfile",
-        "linalg",
-        "linalg.blas",
-        "linalg.cython_blas",
-        "linalg.lapack",
-        "linalg.cython_lapack",
-        "linalg.interpolative",
-        "ndimage",
-        "odr",
-        "optimize",
-        "optimize.elementwise",
-        "signal",
-        "signal.windows",
-        "sparse",
-        "sparse.linalg",
-        "sparse.csgraph",
-        "spatial",
-        "spatial.distance",
-        "spatial.transform",
-        "special",
-        "stats",
-        "stats.contingency",
-        "stats.distributions",
-        "stats.mstats",
-        "stats.qmc",
-        "stats.sampling",
-    ]
-]
-
 
 skip_errors = [
     "GL01",
@@ -83,21 +39,55 @@ skip_errors = [
     "SA04",
     "EX01",  # remove when gh-7168 is resolved
 ]
+encountered_types = set()
+
+
+def walk_module(module_str: str, public_api: list[str] | None = None) -> list[str]:
+    if public_api is None:
+        public_api = []
+
+    module = importlib.import_module(module_str)
+    try:
+        all_ = module.__all__
+    except AttributeError:
+        print(f"No __all__ variable found in public module {module_str!r}")
+        return public_api
+
+    if module_str not in public_api:
+        public_api.append(module_str)
+    for item_str in all_:
+        item = getattr(module, item_str)
+        encountered_types.add(type(item))
+        if isinstance(item, ModuleType):
+            walk_module(f"{module_str}.{item_str}", public_api)
+        elif isinstance(item, type):
+            public_api.append(f"{module_str}.{item_str}")
+            walk_class(module_str, item, public_api)
+            # ... # Handle a custom type
+        else:
+            public_api.append(f"{module_str}.{item_str}")
+
+    return public_api
+
+
+def walk_class(module_str: str, class_: type, public_api: list[str]) -> list[str]:
+    class_str = class_.__name__
+    attrs = [a for a in dir(class_) if not a.startswith("_")]
+    for attr_str in attrs:
+        attr = getattr(class_, attr_str)
+        try:
+            _ = attr.__doc__
+            public_api.append(f"{module_str}.{class_str}.{attr_str}")
+        except AttributeError:
+            pass  # attribute doesn't have a docstring
+    return public_api
 
 
 def main():
     # load in numpydoc config
-    to_check = []
-
-    # get a list of all public objects
-    for module in PUBLIC_MODULES:
-        mod = importlib.import_module(module)
-        try:
-            to_check.extend(obj for f in mod.__all__ if (obj := f"{module}.{f}") not in PUBLIC_MODULES)
-        except AttributeError:
-            # needed for some deprecated modules
-            continue
-
+    pprint(walk_module("parcels"))
+    pprint(encountered_types)
+    return
     errors = 0
     for item in to_check:
         try:
